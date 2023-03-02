d4352238c7a086b45abece7d320b107badb24a6f
Emeghiebo Chizoba <77364360+chizoba-009@users.noreply.github.com>
2023-03-02 22:25:25 +0100

    continued design

---
+++ main.js
@@ -0,0 +1,34 @@
+const images = document.querySelectorAll('.mini-img')
+const mainImg = document.querySelector('.main-img')
+const mainImage = document.querySelectorAll('.main-image')
+const minus = document.querySelector('.minus')
+const add = document.querySelector('.add')
+const addCart = document.querySelector('.addToCart')
+let sum = document.querySelector('.sum')
+
+// EVENY LISTENERS
+minus.addEventListener('click', minusOne)
+add.addEventListener('click', addOne)
+addCart.addEventListener('click', addToCart)
+
+images.forEach((img) => {
+  img.addEventListener('click', () => {
+    mainImage[0].firstElementChild.src = img.src
+  })
+})
+
+function addToCart() {
+  sum.innerHTML++
+}
+
+function addOne() {
+  sum.innerHTML++
+}
+
+function minusOne() {
+  sum.innerHTML--
+
+  if (sum.innerHTML < 0) {
+    sum.innerHTML = 0
+  }
+}
